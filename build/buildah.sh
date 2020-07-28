159ae06ef4ca3230fd13abcc6c50a535c03098fd
Lukas Ramm <lukas.ramm.1998@gmail.com>
2020-07-28 13:08:33 +0200

    fixed healthcheck and tested build

--- build/buildah.sh
+++ build/buildah.sh
@@ -10,18 +10,12 @@ buildah config --label maintainer="lRamm <lukas.ramm.1998@gmail.com>" $container
 
 # ---
 
-# youtube-dl-server
-#buildah run $container -- apk add --no-cache --update git
-#buildah run $container -- cd /usr/src
-#buildah run $container -- git clone https://github.com/xXluki98Xx/youtube-dl-server.git
-
-# ---
-
 # Installing Software
-buildah run $container -- apk add --no-cache --update bash ffmpeg tzdata axel wget git
+buildah run $container -- apk add --no-cache --update bash ffmpeg tzdata axel wget curl
 buildah run $container -- mkdir -p /usr/src/youtube-dl-server
 buildah run $container -- git clone https://github.com/xXluki98Xx/youtube-dl-server.git /usr/src/youtube-dl-server
-buildah run $container -- pip install --no-cache-dir -r /usr/src/youtube-dl-server/requirements.txt
+#buildah copy $container ../../youtube-dl-server /usr/src/youtube-dl-server
+buildah run $container -- pip install --no-cache-dir -r /usr/src/youtube-dl-server/build/requirements.txt
 buildah commit $container
 
 # ---
@@ -32,9 +26,9 @@ buildah config \
 	--healthcheck-retries 3 \
 	--healthcheck-interval 10s \
 	--healthcheck-start-period 30s \
-	--healthcheck "wget -q -t1 -o /dev/null http://localhost:8080 || exit 1" \
+	--healthcheck "curl http://0.0.0.0:8080 || exit 1" \
 	--workingdir /usr/src/youtube-dl-server \
-	--cmd entrypoint.sh \
+	--cmd ./entrypoint.sh \
 	--port 8080/tcp \
 	$container
 
