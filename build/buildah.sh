904675d129dca77fb1da7ad3aebb4c17f458c0f1
Lukas Ramm <lukas.ramm.1998@gmail.com>
2020-07-31 13:31:10 +0200

    fixing buildah script

--- build/buildah.sh
+++ build/buildah.sh
@@ -14,7 +14,7 @@ buildah config --label maintainer="lRamm <lukas.ramm.1998@gmail.com>" $container
 buildah run $container -- apk add --no-cache --update bash ffmpeg tzdata axel git curl
 buildah run $container -- mkdir -p /usr/src/youtube-dl-server
 buildah run $container -- git clone https://github.com/xXluki98Xx/youtube-dl-server.git /usr/src/youtube-dl-server
-buildah run $container -- bash -c "cd /usr/src/youtube-dl-server && git checkout feature_torrent"
+#buildah run $container -- bash -c "cd /usr/src/youtube-dl-server && git checkout feature"
 buildah run $container -- pip3 install --upgrade pip --no-cache-dir -r /usr/src/youtube-dl-server/build/requirements.txt
 buildah commit $container
 
@@ -32,4 +32,5 @@ buildah config \
 	--port 8080/tcp \
 	$container
 
-buildah commit --format docker $container ydl-torrent
+buildah commit --format docker $container youtube-dl-server
+#buildah commit --format docker $container ydl-feature
