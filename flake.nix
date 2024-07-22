3d80a65edad66ea1a3245f12ed0631c755b14fcd
lRamm <lramm.dev@gmail.com>
2024-07-22 19:35:15 +0200

    feat(shell): add inital flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, }:
+  let
+    system =  "x86_64-linux";
+    pkgs = import nixpkgs{inherit system;};
+  in {
+  
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          python3
+          python312Packages.pip
+        ];
+        shellHook =''
+        '';
+      };
+    };
+  
+  };
+
+}
