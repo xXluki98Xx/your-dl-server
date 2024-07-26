f881ada46ebf55087710a910c0c3d62f7c2608f2
lukas.ramm <lukas.ramm@levelbuild.com>
2024-07-27 00:12:48 +0200

    feat(cicd): add dependabot and update pip libs

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'requests==2.25.1',
+        'requests[socks]==2.32.3',
         'click>=8.1.3',
-        'beautifulsoup4==4.12.2',
-        'safer==4.8.0',
+        'beautifulsoup4==4.12.3',
+        'safer==4.12.3',
         'bottle==0.12.25',
-        'yt-dlp==2023.7.6',
+        'yt-dlp==2024.7.25',
         'ffmpeg-python==0.2.0',
-        'stem==1.8.2',
-        'requests[socks]==2.25.1'
+        'stem==1.8.2'
     ],
     entry_points={
         'console_scripts': [
